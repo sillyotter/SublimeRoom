4e516b5b75ef9c28e9a95b096c1db2a981864b03
Guy Oliver <guy.oliver@gmail.com>
2012-10-06 11:46:34 -0400

    Made selectin easier

--- KeepCursorCenteredEventListener.py
+++ KeepCursorCenteredEventListener.py
@@ -5,7 +5,9 @@ class KeepCursorCenteredEventListener(sublime_plugin.EventListener):
 	def on_selection_modified(self, view):
 		try:
 			if (view.settings().get(SublimeRoomShared.key, False)):
-				view.show_at_center(view.sel()[0])
+				sel = view.sel()[0]
+				if sel.a == sel.b:
+					view.show_at_center(view.sel()[0])
 		except:
 			pass
 
